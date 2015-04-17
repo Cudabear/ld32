310d4af1be72e27c12c0f17474fd1566f08e107e
Ryan Graef <cudascubby@gmail.com>
2015-04-16 22:50:34 -0500

    Clean workbench, ready for the big weekend!

---
+++ app.js
@@ -0,0 +1,29 @@
+var WIDTH = 1040;
+var HEIGHT = 630;
+var game;
+
+var config = {
+
+}
+
+window.onload = function(){
+	game = new Phaser.Game(WIDTH, HEIGHT, Phaser.OPENGL, 'game', {preload: preload, create: create, update: update, render: render});
+}
+
+function preload(){
+	
+}
+
+function create(){
+	
+}
+
+function update(){
+	
+}
+
+function render(){
+
+}
+
+
